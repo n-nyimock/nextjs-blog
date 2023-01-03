53f2004efa829fad5bf7cb39a7649553b9931987
n-nyimock <98603598+n-nyimock@users.noreply.github.com>
2023-01-03 12:47:49 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-import Link from 'next/link';
-import Head from  'next/head';
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <div className={styles.container}> 
-  <Head>
+const ProfileDefault = () => {
+    return <div>THIS IS THE PROFILE PAGE</div>;
 
-    <title> My Official CV </title>
-
-  </Head>
-
-
-
-<h1 className={styles.Everest}>This is my official Resume kindly use the navigations at the top to find what you need.
-    </h1>
-  </div>;
- 
-}
+};
+export default ProfileDefault;
